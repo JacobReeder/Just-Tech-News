a57e3c979ff393ce11705f6f71e26b7db4ef0a1d
JacobReeder <jacob.reeder1@gmail.com>
2022-04-18 19:54:32 -0700

    packages installed

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+//Import Sequelize constructor from the library.
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+//Create connection to our datbase and pass in MySQL info.
+const sequelize = new Sequelize(oricess.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
