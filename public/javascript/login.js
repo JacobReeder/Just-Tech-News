a18a2343f7829f94f89c0604c781f454c5967fda
JacobReeder <jacob.reeder1@gmail.com>
2022-04-27 19:25:08 -0700

    Partials added

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,27 @@
-const { response } = require("express");
+
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
 
  async function signupFormHandler(event) {
     event.preventDefault();
@@ -27,29 +50,7 @@ if(response.ok) {
  }
 }
 
-async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
+
   
   
 
